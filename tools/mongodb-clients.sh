b7953ed4541d87c7bebd662f5648c1d02825a3f7
Aymen Mehri <aymen.esprit@gmail.com>
2018-09-22 11:55:23 +0200

    add install mongo compass

--- tools/mongodb-clients.sh
+++ tools/mongodb-clients.sh
@@ -2,3 +2,7 @@
 
 # install mongo client
 sudo apt-get install -y mongodb-clients
+
+# install mongo compass
+wget https://downloads.mongodb.com/compass/mongodb-compass_1.15.1_amd64.deb
+sudo dpkg -i mongodb-compass_1.15.1_amd64.deb
